d28e0b108806b4cddddc13136e1c762622768292
Dane MacMillan <work@danemacmillan.com>
2015-10-08 17:54:06 -0400

    Add skeleton for Add to Cart hijax skeleton.

--- src/app/code/local/Linus/Common/Model/Observer.php
+++ src/app/code/local/Linus/Common/Model/Observer.php
@@ -7,5 +7,17 @@
  */
 class Linus_Common_Model_Observer
 {
+    /**
+     * Observe adding item to cart event.
+     *
+     * Hijax the event so it responds to asynchronous requests.
+     *
+     * @TODO Dane - Look at oemparts integration.
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function onControllerActionPredispatchCheckoutCartAdd(Varien_Event_Observer $observer)
+    {
 
+    }
 }
