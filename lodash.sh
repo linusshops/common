e49aabf3a4cff5723b0fe4c748c0cfbbc5744388
Dane MacMillan <work@danemacmillan.com>
2016-01-06 15:37:23 -0500

    Add methods to lodash build.

--- lodash.sh
+++ lodash.sh
@@ -1,1 +1,3 @@
-lodash strict -o ./src/js/linuscommon/lodash.custom.min.js -p include=noConflict,has,get,size,memoize,defaultsDeep
+#!/usr/bin/env bash
+
+lodash strict -o ./src/js/linuscommon/lodash-3.10.1.custom.min.js -p include=noConflict,has,get,size,memoize,defaultsDeep,isNumber,each,forEach
