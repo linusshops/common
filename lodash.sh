88b3fe37c5a4f3f441309e3b99f4fd855ca1c52d
Sam Schmidt <samuel@dersam.net>
2016-01-22 13:47:26 -0500

    Add _.template to lodash build.

--- lodash.sh
+++ lodash.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-lodash strict -o ./src/js/linuscommon/lodash-3.10.1.custom.min.js -p include=noConflict,has,get,size,memoize,defaultsDeep,isNumber,each,forEach,trim,capitalize,startsWith,delay,isArray,map
+lodash strict -o ./src/js/linuscommon/lodash-3.10.1.custom.min.js -p include=noConflict,has,get,size,memoize,defaultsDeep,isNumber,each,forEach,trim,capitalize,startsWith,delay,isArray,map,template
