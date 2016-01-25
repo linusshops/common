3d4ef32847a98556f530e8cbcbcd2bdffcea410e
Dane MacMillan <work@danemacmillan.com>
2016-01-25 16:42:54 -0500

    Add lodash _.split method.

--- lodash.sh
+++ lodash.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-lodash strict -o ./src/js/linuscommon/lodash-3.10.1.custom.min.js -p include=noConflict,has,get,size,memoize,defaultsDeep,isNumber,each,forEach,trim,capitalize,startsWith,delay,isArray,map,template
+lodash strict -o ./src/js/linuscommon/lodash-3.10.1.custom.min.js -p include=\
+noConflict,\
+has,\
+get,\
+size,\
+memoize,\
+defaultsDeep,\
+isNumber,\
+each,\
+forEach,\
+trim,\
+capitalize,\
+startsWith,\
+delay,\
+isArray,\
+map,\
+template,\
+partial,\
+isUndefined,\
+partialRight, \
+split
+
