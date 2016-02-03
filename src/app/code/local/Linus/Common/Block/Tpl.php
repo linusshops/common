c3bf6c28c31ea13dfe28e202402ab081b847dde6
Sam Schmidt <samuel@dersam.net>
2016-02-03 15:55:27 -0500

    Document tpl block helper methods.

--- src/app/code/local/Linus/Common/Block/Tpl.php
+++ src/app/code/local/Linus/Common/Block/Tpl.php
@@ -8,6 +8,12 @@
  */
 class Linus_Common_Block_Tpl extends Mage_Core_Block_Template
 {
+    /**
+     * Depending on the value of render_mode, if we are in 'tpl', echo
+     * the first parameter, otherwise echo the second.
+     * @param string $tplEchoValue
+     * @param string $defaultEchoValue
+     */
     public function ifTpl($tplEchoValue, $defaultEchoValue)
     {
         if ($this->isTplMode()) {
@@ -17,16 +23,30 @@ class Linus_Common_Block_Tpl extends Mage_Core_Block_Template
         }
     }
 
+    /**
+     * Inject tpl tag to open a JS foreach function for iteration
+     * @param string $collectionVariable the name of the collection to iterate
+     * from the data passed to the template.
+     * @param string $itemName name of the instance from the collection on a
+     * given iteration
+     */
     public function each($collectionVariable='items', $itemName='item')
     {
         echo "{{% _.forEach($collectionVariable, function($itemName){ }}";
     }
 
+    /**
+     * Close an iteration function opened by each.
+     */
     public function endeach()
     {
         echo "{{% }); }}";
     }
 
+    /**
+     * Detect if the block is rendering in tpl or magento mode.
+     * @return bool
+     */
     public function isTplMode()
     {
         return $this->getRenderMode() == 'tpl';
