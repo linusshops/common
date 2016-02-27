52ad22cd6eeb6bfdee9e1b709d30e3a908b30e4e
Dane MacMillan <work@danemacmillan.com>
2016-02-26 19:13:24 -0500

    Add Common front action abstract Controller class for all controllers to extend.

---
+++ src/app/code/local/Linus/Common/Controller/FrontActionAbstract.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * All custom front controllers should extend this, not Mage.
+ *
+ * This will allow all front controllers to automatically have access to the
+ * basic helpers and models provided by Linus_Common.
+ */
+abstract class Linus_Common_Controller_FrontActionAbstract extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Quick access to linus_common/data helpers.
+     *
+     * @return Linus_Common_Helper_Data
+     */
+    public function Common()
+    {
+        return Mage::helper('linus_common');
+    }
+
+    /**
+     * Quick access to linus_common/request helpers.
+     *
+     * @return Linus_Common_Helper_Request
+     */
+    public function CommonRequest()
+    {
+        return Mage::helper('linus_common/request');
+    }
+}
